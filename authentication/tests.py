c66d4b8dbb0167a353136e7fa5277c94104a6cd2
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2023-01-17 10:32:18 +0800

    Test the create_user function

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from authentication.models import User
+from authentication.services import create_user
+
+
+@pytest.mark.django_db
+def test_can_create_a_user():
+    user = create_user(email="john@example.com")
+    assert type(user) == User
